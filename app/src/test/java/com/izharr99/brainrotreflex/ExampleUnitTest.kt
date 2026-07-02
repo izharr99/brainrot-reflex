72c4aa9e0c016ac7b4946aea81ec3a58fded2f8a
Izhar <muhammad.izhar11@gmail.com>
2026-07-02 21:19:55 +0500

    Scaffold Android project with Jetpack Compose
    Fresh Kotlin/Compose Android Studio project structure (Gradle 9.4.1, AGP 9.2.1, Compose BOM 2025.06.00).

---
+++ app/src/test/java/com/izharr99/brainrotreflex/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.izharr99.brainrotreflex
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
